26523530f7260ff27870d1a7b6e93c38866b4bc8
Jason Malcolm-Fey <jason.m.malcolmfey@student.wwcc.edu>
2026-04-09 15:35:58 -0700

    first assignment complete

---
+++ week1/hello_git.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My first Git-tracked C++ program" << "\n";
+    //tells system to print a given message. part of standard library
+    return 0;
+}
